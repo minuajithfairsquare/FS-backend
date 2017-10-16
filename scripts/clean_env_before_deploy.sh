906f0219256eaa33315b389349d35768b4e17c4f
minuajithfairsquare <32395019+minuajithfairsquare@users.noreply.github.com>
2017-10-16 14:37:49 +0100

    Update clean_env_before_deploy.sh

--- scripts/clean_env_before_deploy.sh
+++ scripts/clean_env_before_deploy.sh
@@ -5,6 +5,8 @@ if [[ -n  $isExistApp ]]; then
     systemctl stop apache2
 fi
 # clear dir contents b4 code deploy extract...
-rm -fR /var/www/html/uat/*
-mkdir -p /var/www/html/uat
+rm -fR /var/www/html/harlibuat/*
+mkdir -p /var/www/html/harlibuat
+rm -fR /var/www/html/admin/*
+mkdir -p /var/www/html/admin
 
